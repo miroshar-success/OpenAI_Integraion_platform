76a33eb8294b58391fe1308835ed9d310528233a
oskarmast <oskarmast9436@gmail.com>
2024-04-07 22:03:57 +0200

    req1: change default application image

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -5,5 +5,7 @@ export const DEFAULT_USER_AVATAR = '😀';
 export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
 export const DEFAULT_AGENT_META: MetaData = {};
 export const DEFAULT_INBOX_AVATAR = '🤯';
+// export const DEFAULT_USER_AVATAR_URL =
+//   'https://registry.npmmirror.com/@lobehub/assets-logo/1.2.0/files/assets/logo-3d.webp';
 export const DEFAULT_USER_AVATAR_URL =
-  'https://registry.npmmirror.com/@lobehub/assets-logo/1.2.0/files/assets/logo-3d.webp';
+  'https://registry.npmmirror.com/@lobehub/assets-emoji/1.3.0/files/assets/smiling-face-with-heart-eyes.webp';
