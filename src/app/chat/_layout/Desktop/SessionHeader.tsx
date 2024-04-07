ce9da204dc3141921d13533b38e8437b84882a5f
oskarmast <oskarmast9436@gmail.com>
2024-04-07 15:29:45 +0200

    update0407

--- src/app/chat/_layout/Desktop/SessionHeader.tsx
+++ src/app/chat/_layout/Desktop/SessionHeader.tsx
@@ -1,13 +1,9 @@
-import { ActionIcon } from '@lobehub/ui';
 import { createStyles } from 'antd-style';
-import { MessageSquarePlus } from 'lucide-react';
+import Image from 'next/image';
 import { memo } from 'react';
-import { useTranslation } from 'react-i18next';
 import { Flexbox } from 'react-layout-kit';
 
-import { DESKTOP_HEADER_ICON_SIZE } from '@/const/layoutTokens';
 import SyncStatusTag from '@/features/SyncStatusInspector';
-import { useSessionStore } from '@/store/session';
 
 import SessionSearchBar from '../../features/SessionSearchBar';
 
@@ -20,27 +16,16 @@ export const useStyles = createStyles(({ css, token }) => ({
     top: 0;
   `,
 }));
-
 const Header = memo(() => {
   const { styles } = useStyles();
-  const { t } = useTranslation('chat');
-  const [createSession] = useSessionStore((s) => [s.createSession]);
 
   return (
     <Flexbox className={styles.top} gap={16} padding={16}>
       <Flexbox distribution={'space-between'} horizontal>
         <Flexbox align={'center'} gap={4} horizontal>
-          {/* <Logo className={styles.logo} size={36} type={'text'} /> */}
-          <img height={50} src="images/logo1.png" width={50} />
+          <Image alt={'logo'} height={50} src="images/logo1.png" width={50} />
           <SyncStatusTag />
         </Flexbox>
-        <ActionIcon
-          icon={MessageSquarePlus}
-          onClick={() => createSession()}
-          size={DESKTOP_HEADER_ICON_SIZE}
-          style={{ flex: 'none' }}
-          title={t('newAgent')}
-        />
       </Flexbox>
       <SessionSearchBar />
     </Flexbox>
