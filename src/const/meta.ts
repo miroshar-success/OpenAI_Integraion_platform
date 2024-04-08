72f6f3a69fc96016749ccc92141920a80b4a7af8
oskarmast <oskarmast9436@gmail.com>
2024-04-08 22:29:28 +0200

    update-0408

--- src/const/meta.ts
+++ src/const/meta.ts
@@ -4,8 +4,7 @@ export const DEFAULT_AVATAR = '🤖';
 export const DEFAULT_USER_AVATAR = '😀';
 export const DEFAULT_BACKGROUND_COLOR = 'rgba(0,0,0,0)';
 export const DEFAULT_AGENT_META: MetaData = {};
-export const DEFAULT_INBOX_AVATAR = '🤯';
+export const DEFAULT_INBOX_AVATAR = '/images/icone.png';
 // export const DEFAULT_USER_AVATAR_URL =
 //   'https://registry.npmmirror.com/@lobehub/assets-logo/1.2.0/files/assets/logo-3d.webp';
-export const DEFAULT_USER_AVATAR_URL =
-  'https://registry.npmmirror.com/@lobehub/assets-emoji/1.3.0/files/assets/smiling-face-with-heart-eyes.webp';
+export const DEFAULT_USER_AVATAR_URL = '/images/icone.png';
