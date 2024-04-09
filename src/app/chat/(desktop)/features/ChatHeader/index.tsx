f6a9935aaa7e5f546d778fa56f8c343631f2d338
oskarmast <oskarmast9436@gmail.com>
2024-04-09 21:04:43 +0200

    chat-header-right-hidden

--- src/app/chat/(desktop)/features/ChatHeader/index.tsx
+++ src/app/chat/(desktop)/features/ChatHeader/index.tsx
@@ -1,9 +1,10 @@
 import { ChatHeader } from '@lobehub/ui';
 import { memo } from 'react';
 
-import HeaderAction from './HeaderAction';
+// import HeaderAction from './HeaderAction';
 import Main from './Main';
 
-const Header = memo(() => <ChatHeader left={<Main />} right={<HeaderAction />} />);
+// const Header = memo(() => <ChatHeader left={<Main />} right={<HeaderAction />} />);
+const Header = memo(() => <ChatHeader left={<Main />} />);
 
 export default Header;
